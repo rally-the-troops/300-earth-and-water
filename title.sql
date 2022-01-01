d71fadfed14185afaf90a09e188c51ec248d5bbf
Tor Andersson <tor@ccxvii.net>
2022-01-01 01:36:43 +0100

    Split into separate module.

---
+++ title.sql
@@ -0,0 +1,3 @@
+insert or ignore into titles ( title_id, title_name, bgg ) values ( '300-earth-and-water', '300: Earth & Water', 267058 );
+insert or ignore into roles values ( '300-earth-and-water', 'Persia' );
+insert or ignore into roles values ( '300-earth-and-water', 'Greece' );
